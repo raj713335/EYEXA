1af05181f3eadfaf535f5ff34faa8bc448d8979d
ARNAB DAS <raj713335@gmail.com>
2020-07-11 03:44:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -118,17 +118,7 @@ def mainc():
 
 	face_detector_caffe=base_dir+'/face_detector/res10_300x300_ssd_iter_140000.caffemodel'
 
-	# construct the argument parser and parse the arguments
-	# ap = argparse.ArgumentParser()
-	# ap.add_argument("-f", "--face", type=str,
-	# 	default="face_detector",
-	# 	help="path to face detector model directory")
-	# ap.add_argument("-m", "--model", type=str,
-	# 	default="mask_detector.model",
-	# 	help="path to trained face mask detector model")
-	# ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	# 	help="minimum probability to filter weak detections")
-	# args = vars(ap.parse_args())
+	
 
 
 	# load our serialized face detector model from disk
