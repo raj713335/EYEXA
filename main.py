2db41ebd6993ce1e290fa14bb938ef7876f2bea0
ARNAB DAS <raj713335@gmail.com>
2020-07-19 11:02:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -118,7 +118,17 @@ def mainc():
 
 	face_detector_caffe=base_dir+'/face_detector/res10_300x300_ssd_iter_140000.caffemodel'
 
-	
+	# construct the argument parser and parse the arguments
+	# ap = argparse.ArgumentParser()
+	# ap.add_argument("-f", "--face", type=str,
+	# 	default="face_detector",
+	# 	help="path to face detector model directory")
+	# ap.add_argument("-m", "--model", type=str,
+	# 	default="mask_detector.model",
+	# 	help="path to trained face mask detector model")
+	# ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	# 	help="minimum probability to filter weak detections")
+	# args = vars(ap.parse_args())
 
 
 	# load our serialized face detector model from disk
@@ -132,7 +142,9 @@ def mainc():
 	# initialize the video stream and allow the camera sensor to warm up
 	print("[INFO] starting video stream...")
 	vs = VideoStream(src=0).start()
-	time.sleep(2.0)
+	#time.sleep(2.0)
+
+
 
 
 
@@ -147,7 +159,7 @@ def mainc():
 		# grab the frame from the threaded video stream and resize it
 		# to have a maximum width of 400 pixels
 		frame = vs.read()
-		frame = imutils.resize(frame, width=800)
+		frame = imutils.resize(frame, width=1200)
 
 		resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
@@ -232,19 +244,53 @@ def mainc():
 						cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 		text = "Social Distancing Violations: {}".format(len(nsd))
-		cv2.putText(frame, text, (10, frame.shape[0] - 25),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+		cv2.putText(frame, text, (660, frame.shape[0] - 45),
+					cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4)
+
+		cv2.putText(frame, "EYEXA Social Distancing AND MASK Analyser for COVID-19", (140, 45),
+					cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+		cv2.rectangle(frame, (20, 60), (1170, 100), (170, 170, 170), 2)
+		cv2.putText(frame, "RISK ANALYSIS", (30, 85),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
+		cv2.putText(frame, "-- GREEN : SAFE", (500, 85),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+		cv2.putText(frame, "-- RED: UNSAFE", (1000, 85),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+
+		tot_str = "TOTAL COUNT: " + str(len(idxs))
+		high_str = "HIGH RISK COUNT: " + str(len(nsd))
+		low_str = "LOW RISK COUNT: " + str(0)
+		safe_str = "SAFE COUNT: " + str(len(idxs)-len(nsd))
+
+		sub_img = frame[H - 270: H , 0:240]
+		black_rect = np.ones(sub_img.shape, dtype=np.uint8) * 0
+
+		res = cv2.addWeighted(sub_img, 0.8, black_rect, 0.2, 1.0)
+
+		frame[H - 270:H, 0:240] = res
+
+		cv2.putText(frame, tot_str, (10, H - 235),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+		cv2.putText(frame, safe_str, (10, H - 200),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+		cv2.putText(frame, low_str, (10, H - 165),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 120, 255), 2)
+		cv2.putText(frame, high_str, (10, H - 130),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 150), 2)
 
 		#cv2.imshow("Social Distancing Detector", frame)
 
+		cv2.rectangle(frame, (10, H-100 ), (600, H-10), (170, 170, 170), 2)
+		cv2.putText(frame, "MASK DETECTION ANALYSIS", (40, H-40),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 0), 2)
+		cv2.putText(frame, "-- RED : NO MASK", (420, H-70),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+		cv2.putText(frame, "-- GREEN : MASK", (420, H-35),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-
-
-
-
-
-
-
+		# cv2.putText(frame, "--    GREEN: SAFE", (565, 150),
+		# 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
 		# detect faces in the frame and determine if they are wearing a
 		# face mask or not
@@ -275,8 +321,17 @@ def mainc():
 		cv2.imshow('frame', frame)
 		key = cv2.waitKey(1) & 0xFF
 		# if the `q` key was pressed, break from the loop
+
+	# if writer is None:
+	# 	fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+	# 	writer = cv2.VideoWriter("output.mp4", fourcc, 30,
+	# 							 (frame.shape[1], frame.shape[0]), True)
+
 		if key == ord("q"):
 			break
+
+
+
 	# do a bit of cleanup
 	cv2.destroyAllWindows()
 	vs.stop()
